869ee6f8bf496dcd54d6272c6f64c87f982e8d0f
Maria Kushnareva <kushnareva.msee@gmail.com>
2022-12-25 15:12:02 +0300

    style: зафиксировать размер карточек и другие поправки

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,22 @@
 const editButton = document.querySelector('.profile__edit-button');
 const editPopup = document.querySelector('.popup');
 const editCloseButton = editPopup.querySelector('.popup__close-button');
-const submitButtonPopup = editPopup.querySelector('.popup__submit-button');
-const nameInput = editPopup.querySelector('.popup__field_name');
-const jobInput = editPopup.querySelector('.popup__field_job');
+const nameInput = editPopup.querySelector('.popup__field_type_name');
+const jobInput = editPopup.querySelector('.popup__field_type_job');
 const userName = document.querySelector('.profile__title');
 const userJob = document.querySelector('.profile__subtitle');
 const popupEditForm = document.querySelector('.popup__edit-form');
+
+function closePopup () {
+  editPopup.classList.remove('popup_opened');
+};
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+  closePopup();
+}
 
 editButton.addEventListener('click', (evt) => {
   evt.preventDefault();
@@ -15,23 +25,6 @@ editButton.addEventListener('click', (evt) => {
   jobInput.value = userJob.textContent;
 });
 
-function closePopup () {
-  editPopup.classList.remove('popup_opened');
-};
-
 editCloseButton.addEventListener('click', closePopup);
-
-editPopup.addEventListener('click', (evt) => {
-  if (evt.target === evt.currentTarget){
-    closePopup();
-  };
-});
-
-function handleFormSubmit (evt) {
-  evt.preventDefault();
-  userName.textContent = nameInput.value;
-  userJob.textContent = jobInput.value;
-  closePopup();
-}
 
 popupEditForm.addEventListener('submit', handleFormSubmit);
